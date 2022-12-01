e530b7b7c021cbf83813a143d7b92985f91106c2
lera <leramulugeta@gmail.com>
2022-12-01 18:16:41 +0300

    added @NoArgsConstructor, @AllArgsConstructor, @Data

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,31 +1,14 @@
 package org.example.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class User {
 
     private String login = "standard_user";
     private String password = "secret_sauce";
-
-    public User() {
-    }
-
-    public User(String login, String password) {
-        this.login = login;
-        this.password = password;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
